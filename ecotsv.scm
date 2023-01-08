17163cdaa8429605d0158c4e336de7944814d254
Nevroz Arslan <newrozarslan@gmail.com>
2023-01-08 04:03:01 +0000

    yoruldum

--- ecotsv.scm
+++ ecotsv.scm
@@ -1,9 +1,10 @@
 (import csv-abnf
 	(prefix abnf abnf:)
 	(chicken io)
+	(chicken format)
 	srfi-1
 	utf8
-	
+	trace
 	)
 
 (include "pgnparser-impl.scm")
@@ -13,56 +14,61 @@
 
 (define (err s)
   (print "pgn message error on stream: " s)
-  (list)) ; ; (out)
+  (list)) ;
 
 (define (->char-list s)
   (if (string? s) (string->list s) s))
 
+;;color definitions for output
 (define (red x) (string-append "\x1B[31m" (->string x) "\x1B[0m"))
 (define (green x) (string-append "\x1B[32m" (->string x) "\x1B[0m"))
 (define (yellow x) (string-append "\x1B[33m" (->string x) "\x1B[0m"))
 (define (blue x) (string-append "\x1B[34m" (->string x) "\x1B[0m"))
- (define (magenta x) (string-append "\x1B[35m" (->string x) "\x1B[0m"))
- (define (cyan x) (string-append "\x1B[36m" (->string x) "\x1B[0m"))
+(define (magenta x) (string-append "\x1B[35m" (->string x) "\x1B[0m"))
+(define (cyan x) (string-append "\x1B[36m" (->string x) "\x1B[0m"))
 (define (bold x) (string-append "\x1B[1m" (->string x) "\x1B[0m"))
 (define (underline x) (string-append "\x1B[4m" (->string x) "\x1B[0m"))
 
-
+;;input source
 (define file-to-string(read-string #f (open-input-file "eco_lichess.tsv")))
 
 ;;setup pgn parser
 (define pgnparser
   (lambda (s)
-    (let* ([tokens (pgn car err `(() ,(->char-list s)))])
-      (for-each
-       (lambda (t)
-	 (let* ([token-key (first t)]
-		[token-value (second t)])
-	   (printf "~S ~S" token-key token-value))
-	 (newline)
-	 )
-       tokens))))
+
+    (if (equal? s "pgn") '()
+
+	(let* ([tokens (pgn car err `(() ,(->char-list s)))])
+	  (for-each
+	   (lambda (t)
+	     (display t)
+	    )
+	   tokens)
+	  (newline)
+	  )
+	)
+        ))
+
+(define record-analyze
+  (lambda (record)
+
+	      (let* ([openingName  (second record)]
+		     [pgnSource  (third record)]
+		     [uciSource  (fourth record)]
+		     [fenSource  (fifth record)]
+		     )
+		(pgnparser pgnSource)
+
+		)
+
+  )
+
 
 
 (let ((res (pcsv (->char-list file-to-string))))
-  (for-each (lambda (record)
-	      (let ([openingName  (second record)]
-		    [pgnSource  (third record)]
-		    [uciSource  (fourth record)]
-		    [fenSource  (fifth record)]
-		    )
-		(pgnparser pgnSource)
-		)
+  (for-each 
 
-	      (begin
 
-		(print (underline(blue(second record)))) ;;name
-		(print (underline(red(third record))))   
-		(print (green(fourth record)))
-		(print (fifth record))
-		(newline)
-		)
-	      
-	      )
-	    (map csv-record->list res)))
+   )
+  ))
 

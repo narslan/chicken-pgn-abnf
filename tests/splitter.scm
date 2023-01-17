a02f4fcec6badcafa0a3fc22ea0e2d8aa1a74387
Nevroz Arslan <newrozarslan@gmail.com>
2023-01-17 22:10:43 +0300

    get file positions

--- tests/splitter.scm
+++ tests/splitter.scm
@@ -3,6 +3,7 @@
   (chicken format)
   (chicken port)
   (chicken io)
+  (chicken file posix)
   test
   ;;iterators
   pgn-abnf
@@ -20,29 +21,31 @@
   (let ([fh (open-input-file file)])
     (let loop ([c (read-line fh)]
 	       [k 0]
-	       [sl 0]
+	       [sl '()] ;; contains the positions where the file will be splitted
+	       [nsl 0] ;; how many split points do we have?
 	       )
       (if (eof-object? c)
           (begin
 	    (print "line numbers"  " " k)
-	    (print "char numbers numbers"  " " sl)
+	    (print "split lines"  " " sl)
+	    (print "number of split"  " " nsl)
 	    (close-input-port fh))
 	  (begin
 	    (if (result-line? c)
 		(begin
-		  (print "position last" (port-position fh) )
-		  (print "port: in result line true")
-		  (set! sl (+ sl  (string-length c)))
+		  ;;(print "position last" (file-position fh) )
+		  (set! sl (append sl `(,(file-position fh))))
+		  (set! nsl (+ 1 nsl))
 		  )
 		(begin
-		  (print "position" (port-position fh) )
-		  (print "port: in result line false")
-		  (set! sl (+ sl  (string-length c)))
+		  ;;(print "position" (file-position fh) )
+		  		  
 		  ))
-	    (loop (read-line fh) (+ k 1) sl ))))))
+	    (loop (read-line fh) (+ k 1) sl nsl))))))
 
 
 
-(time (read-it "testdata/1.pgn"))
-;;(time (read-mit "testdata/Adams.pgn"))
+;;(time (read-it "testdata/1.pgn"))
+(time (read-it "testdata/Adams.pgn"))
 
+(test-exit)
